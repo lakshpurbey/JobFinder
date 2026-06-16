b86254ec6a45f981c78e0afe1aa453c089de4d0b
lakshpurbey <laxmipurbey.vis@gmail.com>
2026-06-16 18:10:15 +0545

    Project Intial Setup

---
+++ JobFinder/JobFinder/JobFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobFinderApp.swift
+//  JobFinder
+//
+//  Created by Laksh Purbey on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct JobFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobFinder/JobFinderTests/JobFinderTests.swift
@@ -0,0 +1,19 @@
+//
+//  JobFinderTests.swift
+//  JobFinderTests
+//
+//  Created by Laksh Purbey on 16/06/26.
+//
+
+import Testing
+@testable import JobFinder
+
+struct JobFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
