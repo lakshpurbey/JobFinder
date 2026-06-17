3d59ebb6dcb3689e7fafacf452fb3fa6b4fc06d0
lakshpurbey <laxmipurbey.vis@gmail.com>
2026-06-17 19:06:40 +0545

    Search textfield Design and implemented data

--- JobFinder/JobFinder/Features/JobList/JobListView.swift
+++ JobFinder/JobFinder/Features/JobList/JobListView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct JobListView: View {
     @StateObject var viewModel: JobListViewModel
     
+    @State private var searchText: String = ""
+    
     var body: some View {
         NavigationView {
+
             content
                 .navigationTitle("Jobs")
+                .navigationBarTitleDisplayMode(.large)
+                                .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .automatic))
+                                .onChange(of: searchText) { _, newValue in
+                                    handleSearch(newValue)
+                                }
         }
         .task {
             await viewModel.fetchJobs()
@@ -49,4 +57,14 @@ struct JobListView: View {
             }
         }
     }
+    
+    // MARK: - Search Handler (debounce safe)
+    private func handleSearch(_ text: String) {
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            guard text == searchText else { return } // cancel stale updates
+            viewModel.search(query: text)
+        }
+    }
+
 }
